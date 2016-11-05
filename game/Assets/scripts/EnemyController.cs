b2f16751d88273572b164452f3812c125f7a30e4
Syed Raza <razasyedm@yahoo.com>
2016-11-04 22:37:21 -0400

    update 3
    Replace gameworld objects' flat colors with textures.
    
    Changed the questions segmentation to 4 since there are four questions
    total.
    
    Changed the radius in which the killer can collide with the player. This
    fixed the behaiver when the enemy collids with the player at odd angles.

--- game/Assets/scripts/EnemyController.cs
+++ game/Assets/scripts/EnemyController.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 
+	private Rigidbody2D rb2d;
 	public Transform target;
 	public float speed;
-	private float minDistance = 1f;
+	private float minDistance = .5f; // default value is 1f
 	private float range;
+	private AudioSource caughtThePlayer;
+	private bool isFacingRight;
+	private Animator animator;
 
+	void Awake() {
+		caughtThePlayer = GetComponent<AudioSource>();
+	}
 	// Use this for initialization
 	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		isFacingRight = false;
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		if ((target.position.x - transform.position.x < 0 && isFacingRight) || (target.position.x - transform.position.x > 0 && !isFacingRight))
+			Flip ();
+
+		// Chase the player algorithm
 		range = Vector2.Distance(transform.position, target.position);
 		speed = target.GetComponent<PlayerController> ().speed;
 
@@ -25,16 +39,29 @@ public class EnemyController : MonoBehaviour {
 
 			transform.position = Vector2.MoveTowards(transform.position, target.position, speed * 2 * Time.deltaTime);
 		}
+			
+	
 	}
 
 	// When the player collides with the enemy, kill the player
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.CompareTag ("Player")) {
+			animator.SetBool ("attack", true);
+			// shout is played when killer catches player
+			caughtThePlayer.Play ();
 
 			Debug.Log ("The killer got the player!");
+
+			// Code to handle killing the player
 			//other.gameObject.SetActive (false);
 
 		}
 	}
 		
+	public void Flip() {
+		Vector3 playerScale = transform.localScale;
+		playerScale.x = playerScale.x * -1;
+		transform.localScale = playerScale;
+		isFacingRight = !isFacingRight;
+	}
 }
